1d66751ba0ac8f1a856bfd70a386a7dbb8abc8ef
Adriana Zenck Zimmermann <dricazenck@gmail.com>
2023-01-13 14:26:23 +0100

    add code-conduct js logic (#15)
    Create hide and show logic in code of conduct

---
+++ assets/js/controller.js
@@ -0,0 +1,64 @@
+var controller = (function(jQuery) {
+    var menteeConduct = jQuery("#mentee-conduct");
+    var mentorConduct = jQuery("#mentor-conduct");
+    
+    var btnMenteeConduct = jQuery("#btn-mentee");
+    var btnMentorConduct = jQuery("#btn-mentor");
+    var btnHideMenteesConduct = jQuery("#btn-mentee-hide");
+    var btnHideMentorConduct = jQuery("#btn-mentor-hide");
+    
+
+    var classHide = "d-none";
+
+    var menteeCodeConduct = function() {
+        menteeConduct.removeClass(classHide);
+        btnHideMenteesConduct.removeClass(classHide);
+        btnMenteeConduct.addClass(classHide);
+    }
+
+    var mentorCodeConduct = function() {
+        mentorConduct.removeClass(classHide);
+        btnHideMentorConduct.removeClass(classHide);
+        btnMentorConduct.addClass(classHide);
+    }
+
+    var initPage = function() {
+        btnHideMenteesConduct.addClass(classHide);
+        btnHideMentorConduct.addClass(classHide);
+        btnMenteeConduct.removeClass(classHide);
+        btnMentorConduct.removeClass(classHide);
+
+        mentorConduct.addClass(classHide);
+        menteeConduct.addClass(classHide);
+    };
+
+    var init = function() {
+        initEvents();
+        initPage();
+    };
+
+    var initEvents = function() {
+        btnMenteeConduct.click(function() {
+            menteeCodeConduct();
+        });
+
+        btnMentorConduct.click(function() {
+            mentorCodeConduct();
+        });
+
+        btnHideMenteesConduct.click(function() {
+            initPage();
+        });
+
+        btnHideMentorConduct.click(function() {
+            initPage();
+        });
+    };
+
+    return {
+        init: init
+    };
+
+}(jQuery));
+
+controller.init();
